3b6ec668af51caeb44ef01b0445f9383b2cdefe8
RANJITHKUMAR <ranjithm1193@gmail.com>
2021-05-19 17:40:14 +0530

    [Ranjith] modified Employee_present_not.java

--- Employee_present_not.java
+++ Employee_present_not.java
@@ -5,15 +5,16 @@ class Employee_present_not{
 	public static final int emp_rate_per_hrs=20;
 	public static final int num_of_work=2;
 	public static final int max_hrs_in_month=10;
-	public static void main(String args[]){
+
+	public static int Compute_wage()
+	{
 		int emp_hrs=0;
 		int total_emp_hrs=0;
 		int total_working_day=0;
 		while(total_emp_hrs<=max_hrs_in_month && total_working_day<num_of_work){
-			total_working_day++;	
+			total_working_day++;
 		  	int check=(int)Math.floor(Math.random()*10)%3;
-		  	switch(check)
-			{
+		  	switch(check){
 				case 1: System.out.println("employees is part time hours");
 				emp_hrs=4;
 				break;
@@ -28,5 +29,9 @@ class Employee_present_not{
 		}
 		int total_emp_wages=total_emp_hrs*emp_rate_per_hrs;
 		System.out.println("Total employee wages"+" "+total_emp_wages);
+		return total_emp_wages;
+	}
+public static void main(String args[]){
+	Compute_wage();
 	}
 }
