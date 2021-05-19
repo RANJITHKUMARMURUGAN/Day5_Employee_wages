c7e04b9548be98fa8c02572fefb256e11c68f0a1
RANJITHKUMAR <ranjithm1193@gmail.com>
2021-05-19 15:24:36 +0530

    [Ranjith] modified Employee_present_not.java

--- Employee_present_not.java
+++ Employee_present_not.java
@@ -1,20 +1,16 @@
 import java.util.*;
 class Employee_present_not{
 	public static void main(String args[]){
-	int part_time=1;
-		int isFull_time=2;
+		int isFull_time=1;
 		int emp_rate_per_hrs=20;
 		int emp_hrs=0;
 		int emp_wages=0;
 		double check=Math.floor(Math.random()*10)%2;
-		if(part_time==check){
-			emp_hrs=4;
-		}
-		else if(isFull_time==check){
+		if(isFull_time==check){
 			emp_hrs=8;
 		}
 		else{
-			emp_hrs=0;
+			emp_hrs=4;
 		}
 		emp_wages=emp_hrs*emp_rate_per_hrs;
 		System.out.println(emp_wages);
