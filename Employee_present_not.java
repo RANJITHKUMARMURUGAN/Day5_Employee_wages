fe6670004102cb9eb9f4fa2990e67afe43aaa280
RANJITHKUMAR <ranjithm1193@gmail.com>
2021-05-19 13:59:34 +0530

    [Ranjith] added Employee_present_not.java

---
+++ Employee_present_not.java
@@ -0,0 +1,12 @@
+class Employee_present_not{
+	public static void main(String args[]){
+		int isFull_time=1;
+		double check=Math.floor(Math.random()*10)%2;
+		if(isFull_time==check){
+			System.out.println("Employee is present");
+		}
+		else{
+			System.out.println("Employee is not present");
+		}
+	}
+}
